85294877ca3292da5fa37d21f388dddb796c40f8
arslanrasheedacca <iori.arslan@gmail.com>
2025-02-13 12:27:31 +0500

    Update book_system.js

--- bookSystem/book_system.js
+++ bookSystem/book_system.js
@@ -25,13 +25,18 @@ function addBook(){
     <p><strong>Book Name: </strong>${book.name}</p>
     <p><strong>Author Name: </strong>${book.authorName}</p>
     <p><strong>Book Description: </strong>${book.bookDescription}</p>
-    <p><strong>No. of Pages: </strong>${book.pagesNumber} page(s)</p>`
+    <p><strong>No. of Pages: </strong>${book.pagesNumber} page(s)</p>
+    <button onclick="deletebook(${index})">Delete</button>`
     );
     document.getElementById('books').innerHTML = booksDiv.join('');
  }
- 
+function deletebook(index) {
+    books.splice(index, 1);  // Remove the book at the given index
+    showbooks();  // Refresh the book list
+}
+
  function clearInputs(){
- 						document.getElementById('bookName').value = '';
+	    document.getElementById('bookName').value = '';
             document.getElementById('authorName').value = '';
             document.getElementById('bookDescription').value = '';
             document.getElementById('pagesNumber').value = '';
